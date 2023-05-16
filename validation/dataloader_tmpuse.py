b4c3e00dc742fe952e42b65a1f8e66041d9b348a
Nina Weng <nina.weng@foxmail.com>
2023-05-16 11:01:36 +0200

    -

---
+++ validation/dataloader_tmpuse.py
@@ -0,0 +1,305 @@
+
+import torch
+from torch.utils.data import DataLoader, Dataset
+import pandas as pd
+import numpy as np
+
+import torchvision.transforms as T
+import pytorch_lightning as pl
+from skimage.io import imread
+from PIL import Image
+from tqdm import tqdm
+import os
+
+
+DISEASE_LABELS = [
+            'No Finding',
+            'Enlarged Cardiomediastinum',
+            'Cardiomegaly',
+            'Lung Opacity',
+            'Lung Lesion',
+            'Edema',
+            'Consolidation',
+            'Pneumonia',
+            'Atelectasis',
+            'Pneumothorax',
+            'Pleural Effusion',
+            'Pleural Other',
+            'Fracture',
+            'Support Devices']
+
+class ChexpertDatasetNew(Dataset):
+    def __init__(self, img_data_dir, df_data, image_size, augmentation=False, pseudo_rgb = False,single_label=None):
+        self.img_data_dir = img_data_dir
+        self.df_data = df_data
+        self.image_size = image_size
+        self.do_augment = augmentation
+        self.pseudo_rgb = pseudo_rgb
+        self.single_label = single_label
+
+        self.labels=DISEASE_LABELS
+        if self.single_label is not None:
+            self.labels = [self.single_label]
+
+        self.augment = T.Compose([
+            T.RandomHorizontalFlip(p=0.5),
+            T.RandomApply(transforms=[T.RandomAffine(degrees=15, scale=(0.9, 1.1))], p=0.5),
+        ])
+
+        self.samples = []
+        # for idx, _ in enumerate(tqdm(range(len(self.df_data)), desc='Loading Data')):
+        for idx in tqdm((self.df_data.index), desc='Loading Data'):
+            path_preproc_idx = self.df_data.columns.get_loc("path_preproc")
+            img_path = self.img_data_dir + self.df_data.iloc[idx, path_preproc_idx]
+            img_label = np.zeros(len(self.labels), dtype='float32')
+            for i in range(0, len(self.labels)):
+                img_label[i] = np.array(self.df_data.loc[idx, self.labels[i].strip()] == 1, dtype='float32')
+
+            sample = {'image_path': img_path, 'label': img_label}
+            self.samples.append(sample)
+
+    def __len__(self):
+        return len(self.df_data)
+
+    def __getitem__(self, item):
+        sample = self.get_sample(item)
+
+        # image = torch.from_numpy(sample['image'])
+        image = T.ToTensor()(sample['image'])
+        label = torch.from_numpy(sample['label'])
+
+        # image = torch.permute(image, dims=(2, 0, 1))
+        ### image = image / 255
+
+
+
+        if self.do_augment:
+            image = self.augment(image)
+
+        if self.pseudo_rgb:
+            image = image.repeat(3, 1, 1)
+
+
+
+
+
+        return {'image': image, 'label': label}
+
+    def get_sample(self, item):
+        sample = self.samples[item]
+        # image = imread(sample['image_path']).astype(np.float32)
+        try:
+            image = Image.open(sample['image_path']).convert('RGB') #PIL image
+        except:
+            print('PIL not working on image: {}'.format(sample['image_path']))
+            image = imread(sample['image_path']).astype(np.float32)
+
+
+        return {'image': image, 'label': sample['label']}
+
+    def exam_augmentation(self,item):
+        assert self.do_augment == True, 'No need for non-augmentation experiments'
+
+        sample = self.get_sample(item) #PIL
+        image = T.ToTensor()(sample['image'])
+
+        if self.do_augment:
+            image_aug = self.augment(image)
+
+        image_all = torch.cat((image,image_aug),axis= 1)
+        assert image_all.shape[1]==self.image_size[0]*2, 'image_all.shape[1] = {}'.format(image_all.shape[1])
+        return image_all
+
+
+
+
+
+class CheXpertDataResampleModule(pl.LightningDataModule):
+    def __init__(self, img_data_dir,csv_file_img, image_size, pseudo_rgb, batch_size, num_workers,augmentation,
+                 outdir,version_no,
+                 female_perc_in_training = None,
+                 chose_disease='No Finding',
+                 random_state=None):
+        super().__init__()
+        self.img_data_dir = img_data_dir
+        self.csv_file_img = csv_file_img
+
+        self.outdir = outdir
+        self.version_no = version_no
+
+        # new parameters
+        self.female_perc_in_training = female_perc_in_training
+        assert self.female_perc_in_training in [0,50,100], 'Not implemented female_perc_in_training: {}'.format(self.female_perc_in_training)
+        self.chose_disease = chose_disease # str, one of the labels
+        self.rs = random_state
+        self.male, self.female = 'Male', 'Female'
+        self.genders = [self.female, self.male]
+        self.col_name_patient_id = 'patient_id'
+        self.col_name_gender = 'sex'
+
+        # pre-defined parameter
+        self.num_per_gender = 18000
+        self.disease_pervalence_total,self.disease_pervalence_female, self.disease_pervalence_male = self.get_prevalence()
+        self.perc_train, self.perc_val, self.perc_test = 0.6,0.1,0.3
+        assert self.perc_val+self.perc_test+self.perc_train == 1
+
+
+        df_train,df_valid,df_test = self.dataset_sampling()
+        self.df_train = df_train
+        self.df_valid = df_valid
+        self.df_test = df_test
+
+        if self.df_train is None: return # random state does not provide a enough amount of samples to sample
+
+        self.image_size = image_size
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.augmentation=augmentation
+
+
+
+
+        self.train_set = ChexpertDatasetNew(self.img_data_dir,self.df_train, self.image_size, augmentation=augmentation,
+                                    pseudo_rgb=pseudo_rgb,single_label=self.chose_disease)
+        self.val_set = ChexpertDatasetNew(self.img_data_dir,self.df_valid, self.image_size, augmentation=False,
+                                  pseudo_rgb=pseudo_rgb,single_label=self.chose_disease)
+        self.test_set = ChexpertDatasetNew(self.img_data_dir,self.df_test, self.image_size, augmentation=False,
+                                   pseudo_rgb=pseudo_rgb,single_label=self.chose_disease)
+
+        print('#train: ', len(self.train_set))
+        print('#val:   ', len(self.val_set))
+        print('#test:  ', len(self.test_set))
+
+    def train_dataloader(self):
+        return DataLoader(self.train_set, self.batch_size, shuffle=True, num_workers=self.num_workers)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_set, self.batch_size, shuffle=False, num_workers=self.num_workers)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_set, self.batch_size, shuffle=False, num_workers=self.num_workers)
+
+
+    def dataset_sampling(self):
+        '''
+        doc: https://docs.google.com/document/d/1N1XJWFqF_5CDYkdbbXlzXmtepd-PFPhtKaDXwjKVYH8/edit
+        :param csv_file_img:
+        :return:
+        '''
+        df = pd.read_csv(self.csv_file_img, header=0)
+        for each_l in DISEASE_LABELS:
+            df[each_l] = df[each_l].apply(lambda x: 1 if x == 1 else 0)
+        #patient_id_list = list(set(df['Patient ID'].to_list()))
+        grouped = df.groupby(self.col_name_patient_id)
+        df_per_patient = grouped.apply(lambda x: x.sample(n=1, random_state=self.rs))
+
+
+        train_set, val_set, test_set = None, None, None
+        for each_gender in self.genders:
+            for isDisease in [True, False]:
+                this_df = df_per_patient[(df_per_patient[self.col_name_gender] == each_gender) &
+                                         (df_per_patient[self.chose_disease] == isDisease)]
+                print('{}+{}, number of samples:{}'.format(each_gender, isDisease, len(this_df)))
+
+                p = self.disease_pervalence_female[self.chose_disease] if each_gender==self.female else \
+                    self.disease_pervalence_male[self.chose_disease]
+
+                N = int(self.num_per_gender * p) if isDisease else int(
+                    self.num_per_gender * (1 - p))
+                print('N:{}'.format(N))
+
+                if N > len(this_df):
+                    # not enough samples for keeping the same amount of data per resampling
+                    print('random state {} does not provide with enough disease-labeled samples'.format(self.rs))
+                    return None,None,None
+
+                this_df = this_df.sample(n=N, random_state=self.rs)
+                this_train, this_val, this_test = self.set_split(this_df, self.perc_train,self.perc_val,self.perc_test, self.rs)
+
+                # keep the training set same amount of samples for female_perc_in_training = [0,50,100]
+                if self.female_perc_in_training == 50:
+                    N_train = len(this_train)
+                    N_train_half = int(N_train / 2)
+                    this_train = this_train.sample(n=N_train_half, random_state=self.rs)
+                    # val should keep the same as train
+                    N_val = len(this_val)
+                    N_val_half = int(N_val / 2)
+                    this_val = this_val.sample(n=N_val_half, random_state=self.rs)
+
+                if each_gender == self.female and self.female_perc_in_training != 0:
+                    if train_set is None:
+                        train_set = this_train
+                    else:
+                        train_set = pd.concat([train_set, this_train], axis=0)
+                    # val should keep the same as train
+                    if val_set is None:
+                        val_set = this_val
+                    else:
+                        val_set = pd.concat([val_set, this_val], axis=0)
+
+                if each_gender == self.male and self.female_perc_in_training != 100:
+                    if train_set is None:
+                        train_set = this_train
+                    else:
+                        train_set = pd.concat([train_set, this_train], axis=0)
+                    if val_set is None:
+                        val_set = this_val
+                    else:
+                        val_set = pd.concat([val_set, this_val], axis=0)
+
+                # test set is not influenced by training settingsS
+                if test_set is None:
+                    test_set = this_test
+                else:
+                    test_set = pd.concat([test_set, this_test], axis=0)
+
+        train_set.reset_index(inplace=True,drop=True)
+        val_set.reset_index(inplace=True,drop=True)
+        test_set.reset_index(inplace=True,drop=True)
+
+        # save splits
+        train_set.to_csv(os.path.join(self.outdir, 'train.version_{}.csv'.format(self.version_no)), index=False)
+        val_set.to_csv(os.path.join(self.outdir, 'val.version_{}.csv'.format(self.version_no)), index=False)
+        test_set.to_csv(os.path.join(self.outdir, 'test.version_{}.csv'.format(self.version_no)), index=False)
+
+        return train_set,val_set,test_set
+
+    def get_prevalence(self):
+        df = pd.read_csv(self.csv_file_img, header=0)
+        for each_l in DISEASE_LABELS:
+            df[each_l] = df[each_l].apply(lambda x: 1 if x == 1 else 0)
+        df_per_patient = df.groupby([self.col_name_patient_id , self.col_name_gender]).mean()
+        df_per_patient_p = df_per_patient.mean()[DISEASE_LABELS].to_list()
+
+        df_per_patient_gender_p = df_per_patient.groupby([self.col_name_gender]).mean()[DISEASE_LABELS]
+        df_per_patient_gender_p_male = df_per_patient_gender_p.loc[self.male].to_list()
+        df_per_patient_gender_p_female = df_per_patient_gender_p.loc[self.female].to_list()
+
+        assert len(df_per_patient_gender_p_female) == len(DISEASE_LABELS)
+        assert len(df_per_patient_gender_p_male) == len(DISEASE_LABELS)
+        assert len(df_per_patient_p) == len(DISEASE_LABELS)
+
+        dict_per_patient_p = {}
+        for i,each_l in enumerate(DISEASE_LABELS): dict_per_patient_p[each_l] = df_per_patient_p[i]
+
+        dict_per_patient_gender_p_female = {}
+        for i, each_l in enumerate(DISEASE_LABELS): dict_per_patient_gender_p_female[each_l] = df_per_patient_gender_p_female[i]
+
+        dict_per_patient_gender_p_male = {}
+        for i, each_l in enumerate(DISEASE_LABELS): dict_per_patient_gender_p_male[each_l] = df_per_patient_gender_p_male[i]
+
+        print('Disease prevalence total: {}'.format(dict_per_patient_p))
+        print('Disease prevalence Female: {}'.format(dict_per_patient_gender_p_female))
+        print('Disease prevalence Male: {}'.format(dict_per_patient_gender_p_male))
+
+        return dict_per_patient_p,dict_per_patient_gender_p_female,dict_per_patient_gender_p_male
+
+    def set_split(self,df,train_frac,val_frac,test_frac,rs):
+        test = df.sample(frac=test_frac, random_state=rs)
+
+        # get everything but the test sample
+        train_val = df.drop(index=test.index)
+        train = train_val.sample(frac=train_frac / (train_frac + val_frac), random_state=rs)
+        val = train_val.drop(index=train.index)
+
+        return train, val, test
---
+++ validation/domain_adaptation.py
@@ -0,0 +1,167 @@
+import sys
+sys.path.append('../../NIH_classifer')
+from validation.model_tmpuse import ResNet_chexpert,ResNet_NIH
+from dataloader.dataloader import NIHDataResampleModule
+from validation.dataloader_tmpuse import CheXpertDataResampleModule
+from prediction.disease_prediction import get_cur_version
+import os
+import torch
+from prediction.disease_prediction import test_func
+import pandas as pd
+
+def load_model(dataset_type,ckpt_dir):
+    model_choose = 'resnet'
+    num_classes = 1
+    lr = 1e-5
+    pretrained = True
+    model_scale = '50'
+
+    if model_choose == 'resnet':
+        if dataset_type == 'NIH':
+            model_type = ResNet_NIH
+        elif dataset_type == 'chexpert':
+            model_type = ResNet_chexpert
+
+
+    file_list = os.listdir(ckpt_dir)
+    assert len(file_list) == 1
+    ckpt_path = ckpt_dir + file_list[0]
+    model = model_type.load_from_checkpoint(ckpt_path,
+                                                num_classes=num_classes, lr=lr, pretrained=pretrained,
+                                                model_scale=model_scale,
+                                                )
+
+    return model
+
+
+
+
+
+# ## Hyperparameters: D=Pneumothorax, 0%female, rs=range[0,5]
+def main():
+    run_config_NIH = []
+    run_config_chexpert = []
+    version_no_NIH = []
+    version_no_chexpert = []
+
+    for i in range(5):
+        run_config_NIH.append(
+            'resnet50-lr1e-05-ep20-pt1-aug1-0%female-DPneumothorax-rs{}-imgs224_newval_es_p3'.format(i))
+        run_config_chexpert.append('resnet50-lr1e-05-ep20-pt1-aug1-0%female-DPneumothorax-rs{}-imgs224'.format(i))
+        version_no_NIH.append(0)
+        version_no_chexpert.append(0)
+
+    run_dir_NIH = 'D:\\ninavv\\phd\\research\\run_results\\NIH_results\\disease\\'
+    run_dir_chexpert = 'D:\\ninavv\\phd\\research\\run_results\\chexpert_results\\disease\\'
+
+    run_dir_chexpert = '/work3/ninwe/run/chexpert/disease/'
+    run_dir_NIH = '/work3/ninwe/run/NIH/disease/'
+
+    rs_chose = 0
+
+    ckpt_dir_NIH = run_dir_NIH + '\\' + run_config_NIH[rs_chose] + '\\version_0\\checkpoints\\'
+    NIH_model = load_model('NIH',ckpt_dir_NIH)
+
+    ckpt_dir_chexpert = run_dir_chexpert + '\\' + run_config_chexpert[rs_chose] + '\\version_0\\checkpoints\\'
+    chexpert_model = load_model('chexpert', ckpt_dir_chexpert)
+
+    img_data_dir = '/work3/ninwe/dataset/NIH/preproc_224x224/'
+    csv_file_img = None
+    image_size=(224,224)
+    batch_size=64
+    num_workers=1
+    augmentation=False
+    run_dir = '/work3/ninwe/run/NIH/disease/'
+    run_config='intradataset_rs_{}'.format(rs_chose)
+    out_dir = run_dir + run_config
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    out_dir_NIH = out_dir+'/NIH_datasplit/'
+    if not os.path.exists(out_dir_NIH):
+        os.makedirs(out_dir_NIH)
+
+    cur_version = get_cur_version(out_dir)
+    female_perc_in_training=0
+    chose_disease_str='Pneumothorax'
+
+
+    data_NIH = NIHDataResampleModule(img_data_dir=img_data_dir,
+                                     csv_file_img=csv_file_img,
+                                     image_size=image_size,
+                                     pseudo_rgb=False,
+                                     batch_size=batch_size,
+                                     num_workers=num_workers,
+                                     augmentation=augmentation,
+                                     outdir=out_dir_NIH,
+                                     version_no=cur_version,
+                                     female_perc_in_training=female_perc_in_training,
+                                     chose_disease=chose_disease_str,
+                                     random_state=rs_chose)
+
+    img_data_dir = '/work3/ninwe/dataset/'
+    csv_file_img = '../datafiles/chexpert/' + 'chexpert.sample.csv'
+    out_dir_chexpert= out_dir + '/chexpert_datasplit/'
+    if not os.path.exists(out_dir_chexpert):
+        os.makedirs(out_dir_chexpert)
+
+    cur_version = get_cur_version(out_dir)
+
+    data_chexpert = CheXpertDataResampleModule(img_data_dir=img_data_dir,
+                                csv_file_img=csv_file_img,
+                                image_size=image_size,
+                                pseudo_rgb=False,
+                                batch_size=batch_size,
+                                num_workers=num_workers,
+                                augmentation=augmentation,
+                                outdir=out_dir_chexpert,
+                                version_no=cur_version,
+                                female_perc_in_training=female_perc_in_training,
+                                chose_disease=chose_disease_str,
+                                random_state=rs_chose)
+
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda:" + str(0) if use_cuda else "cpu")
+
+    NIH_model.to(device)
+    chexpert_model.to(device)
+
+    num_classes=1
+    cols_names_classes = ['class_' + str(i) for i in range(0, num_classes)]
+    cols_names_logits = ['logit_' + str(i) for i in range(0, num_classes)]
+    cols_names_targets = ['target_' + str(i) for i in range(0, num_classes)]
+
+    print('1.TESTING NIH test set Using NIH model')
+    preds_test, targets_test, logits_test = test_func(NIH_model, data_NIH.test_dataloader(), device)
+    df = pd.DataFrame(data=preds_test, columns=cols_names_classes)
+    df_logits = pd.DataFrame(data=logits_test, columns=cols_names_logits)
+    df_targets = pd.DataFrame(data=targets_test, columns=cols_names_targets)
+    df = pd.concat([df, df_logits, df_targets], axis=1)
+    df.to_csv(os.path.join(out_dir, 'predictions.testNIHtrainNIH.version_{}.csv'.format(cur_version)), index=False)
+
+    print('2.TESTING NIH test set Using chexpert model')
+    preds_test, targets_test, logits_test = test_func(chexpert_model, data_NIH.test_dataloader(), device)
+    df = pd.DataFrame(data=preds_test, columns=cols_names_classes)
+    df_logits = pd.DataFrame(data=logits_test, columns=cols_names_logits)
+    df_targets = pd.DataFrame(data=targets_test, columns=cols_names_targets)
+    df = pd.concat([df, df_logits, df_targets], axis=1)
+    df.to_csv(os.path.join(out_dir, 'predictions.testNIHtrainChexpert.version_{}.csv'.format(cur_version)), index=False)
+
+    print('3.TESTING chexpert test set Using chexpert model')
+    preds_test, targets_test, logits_test = test_func(chexpert_model, data_chexpert.test_dataloader(), device)
+    df = pd.DataFrame(data=preds_test, columns=cols_names_classes)
+    df_logits = pd.DataFrame(data=logits_test, columns=cols_names_logits)
+    df_targets = pd.DataFrame(data=targets_test, columns=cols_names_targets)
+    df = pd.concat([df, df_logits, df_targets], axis=1)
+    df.to_csv(os.path.join(out_dir, 'predictions.testChexperttrainChexpert.version_{}.csv'.format(cur_version)), index=False)
+
+    print('4.TESTING chexpert test set Using NIH model')
+    preds_test, targets_test, logits_test = test_func(NIH_model, data_chexpert.test_dataloader(), device)
+    df = pd.DataFrame(data=preds_test, columns=cols_names_classes)
+    df_logits = pd.DataFrame(data=logits_test, columns=cols_names_logits)
+    df_targets = pd.DataFrame(data=targets_test, columns=cols_names_targets)
+    df = pd.concat([df, df_logits, df_targets], axis=1)
+    df.to_csv(os.path.join(out_dir, 'predictions.testChexperttrainNIH.version_{}.csv'.format(cur_version)), index=False)
+
+
+if __name__ == '__main__':
+    main()
