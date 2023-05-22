e3e1297af22dea1a811e702174ce7be0bd71c892
Nina Weng <nina.weng@foxmail.com>
2023-05-22 13:04:39 +0200

    new sampling

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -237,7 +237,7 @@ class NIHDataResampleModule(pl.LightningDataModule):
         self.rs = random_state
         self.shuffle = shuffle
         self.num_per_patient= num_per_patient
-        assert self.num_per_patient >=1
+        assert self.num_per_patient >=1 or self.num_per_patient is None
 
         # pre-defined parameter
         self.num_per_gender = 13000
